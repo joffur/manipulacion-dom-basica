8d764b8ab38b87c1d3d789eaec5a060f7188e622
Jose Fernando Fuentes Roa <ing.josefuentes@hotmail.com>
2023-02-02 22:23:44 -0500

    addEventListener

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 //Traer los elementos del HTML según la etiqueta, clase y/o ID
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
-const result = document.querySelector('#result')
+const result = document.querySelector('#result');
 
-function btnOnClick() {
+// form.addEventListener('submit', sumarInputValues);
+
+// function sumarInputValues(event) {
+//   console.log(event);
+//   event.preventDefault();
+//   const sumaInputs = parseFloat(input1.value) + parseFloat(input2.value);
+//   result.innerText = 'Resultado: ' + sumaInputs;
+// }
+
+btn.addEventListener('click', sumarInputValues);
+
+function sumarInputValues(event) {
+  // console.log(event);
+  // event.preventDefault();
   const sumaInputs = parseFloat(input1.value) + parseFloat(input2.value);
   result.innerText = 'Resultado: ' + sumaInputs;
 }
